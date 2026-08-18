d7835e0ddb2601bb1c84d76ca614795c48174f9d
Siddharth <siddharth4403@gmail.com>
2026-08-18 19:39:23 +0530

    addition

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/additon.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class additon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter a number");
+        int num2 = sc.nextInt();
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+    }
+}
+    
+
