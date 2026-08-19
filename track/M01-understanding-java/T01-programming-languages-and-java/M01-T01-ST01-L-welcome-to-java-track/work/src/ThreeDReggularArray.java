a8c62759b8bc5a6d409048d4ed25b8f7a00bc628
Siddharth <siddharth4403@gmail.com>
2026-08-19 16:57:38 +0530

    Three dimentional reggular array

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ThreeDReggularArray.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ThreeDReggularArray.java
@@ -1,51 +1,39 @@
 import java.util.Scanner;
+
 public class ThreeDReggularArray {
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			
-			System.out.println("Enter the No.of schools:- ");
-			int school = scan.nextInt();
-			
-			System.out.println("Enter the No.of classes:- ");
-			int classroom = scan.nextInt(); 
-			
-			System.out.println("Enter the No.of Students:- ");
-			int students=scan.nextInt();
-			
-			int arr[] [][]= new int [school][classroom][students];
-			for (int i = 0; i < arr.length; i++) 
-			{
-				for(int j = 0; j<arr[i].length; j++) 
-				{
-					for (int k=0;k<arr[i][j].length; k++) 
-					{
-					System.out.println("The school-"+(i+1) +" class-"+(j+1)+" student-"+(k+1));
-					arr[i][j][k]= scan.nextInt();
-					}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the No.of schools:- ");
+		int school = scan.nextInt();
+
+		System.out.println("Enter the No.of classes:- ");
+		int classroom = scan.nextInt();
+
+		System.out.println("Enter the No.of Students:- ");
+		int students = scan.nextInt();
+
+		int arr[][][] = new int[school][classroom][students];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				for (int k = 0; k < arr[i][j].length; k++) {
+					System.out.println("The school-" + (i + 1) + " class-" + (j + 1) + " student-" + (k + 1));
+					arr[i][j][k] = scan.nextInt();
 				}
 			}
-			System.out.println("The array elements are:- ");
-			for (int i = 0; i < arr.length; i++)
-			{
-				for(int j = 0; j<arr[i].length; j++)
+		}
+		System.out.println("The array elements are:- ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				for (int k = 0; k < arr[i][j].length; k++)
+
 				{
-					for (int k=0;k<arr[i][j].length; k++) 
-					
-					{
-					System.out.println(arr[i][j][k]+" ");
-					}
-					System.out.print("");
+					System.out.print(arr[i][j][k] + " ");
 				}
 				System.out.println();
 			}
-			
+			System.out.println();
 		}
+
 	}
-
-
-
-
-
-
-    
-
+}
