5478e331ae87c63646fb8442c98799920e03bc89
Siddharth <siddharth4403@gmail.com>
2026-09-07 19:35:39 +0530

    Strings program 2

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/stringsp2.java
@@ -0,0 +1,12 @@
+public class stringsp2 {
+     
+    public static void main(String[] args) {
+        String s1 = "siddharth";
+        String s2 = "siddharth";
+        if (s1 == s2) {
+            System.out.println("BOTH ARE EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+    }
+}
