f86e14fc5cbf30119f3b7f557fe837b5d5494d3d
Siddharth <siddharth4403@gmail.com>
2026-09-07 19:32:29 +0530

    Strings

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/strings.java
@@ -0,0 +1,11 @@
+class strings {
+    public static void main(String[] args) {
+        String s1 = "siddharth";
+        String s2 = "siddhu";
+        if (s1 == s2) {
+            System.out.println("BOTH ARE EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+    }
+}
